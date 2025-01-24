4258162bc755a2bb06eb88990b60c9c581d7eab0
Wafa Makhlouf <69568921+wafamakhlouf@users.noreply.github.com>
2025-01-24 16:11:46 +0100

    Create Hubbard_model.py

---
+++ examples/Hubbard Model.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import os
+import sys 
+import psi4
+import scipy 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import quantnbody as qnb
+import quantnbody.fermionic.tools as tools
+import lpfet
